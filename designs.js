fef136a7b1a4a6ef9c8acf633e9d59d26e0b6286
Denisa28 <36009528+Denisa28@users.noreply.github.com>
2018-01-31 21:43:19 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -4,7 +4,23 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+  var row = $('#input_height').val();
+  var column = $('#input_width').val();
+  var grid = $("#pixel_canvas");
 
-// Your code goes here!
+  grid.children().remove();
 
+  for (var i = 0; i < row; i++) {
+    grid.append('<tr></tr>');
+    for (var j = 0; j < column; j++) {
+      grid.children().last().append('<td></td>');
+    }
+    grid.on('click','td',function() {
+      var color = $('#colorPicker').val();
+      $(this).attr('bgcolor',color); 
+    });
+    grid.on('dblclick','td',function() {
+      $(this).attr('bgcolor','white'); 
+    });
+  }
 }
